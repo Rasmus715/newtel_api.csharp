9c1acf667c64ba17f8b9dc41fe705e130748415c
Ivan Zatisev <i.zatsev@dreamsoft.by>
2022-05-05 15:17:36 +0300

    Initial commit

---
+++ newtel_api.csharp/Controllers/NewtelController.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using newtel_api.csharp.Services;
+
+namespace newtel_api.csharp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NewtelController : ControllerBase
+    {
+        private readonly ILogger<NewtelController> _logger;
+        private readonly INewtelService _newtelService;
+
+        public NewtelController(ILogger<NewtelController> logger,INewtelService newtelService)
+        {
+            _logger = logger;
+            _newtelService = newtelService;
+        }
+
+        [HttpPost]
+        [Route("CallPassword")]
+        public async Task<IActionResult> CallPassword(string phoneNumber)
+        {
+            try
+            {
+                return Ok(await _newtelService.CallPassword(phoneNumber));
+            }
+            catch (HttpRequestException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ newtel_api.csharp/Services/INewtelService.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Security.Cryptography;
+using Microsoft.Extensions.Configuration;
+
+namespace newtel_api.csharp.Services
+{
+    public interface INewtelService
+    {
+        Task<string> CallPassword(string phoneNumber);
+    }
+
+    public class NewtelService : INewtelService
+    {
+        private readonly IConfiguration _configuration;
+        public  NewtelService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<string> CallPassword(string phoneNumber)
+        {
+            //Generate sequence of variables
+            var currentTime = DateTimeOffset.Now.ToUnixTimeSeconds();
+            var generatedPin = new Random().Next(1000, 9999);
+
+            var bodyAsString = new {dstNumber = $"{phoneNumber}", pin = $"{generatedPin}", timeout = 30};
+            var json = JsonConvert.SerializeObject(bodyAsString);
+
+            var source =
+                $"call-password/start-password-call\n{currentTime}\n{_configuration["Newtel:AccessKey"]}\n{json}\n{_configuration["Newtel:WriteKey"]}";
+
+            //Hashsing the string into SHA256
+            using var sha256Hash = SHA256.Create();
+            var sourceBytes = Encoding.UTF8.GetBytes(source);
+            var hashBytes = sha256Hash.ComputeHash(sourceBytes);
+            var hash = BitConverter.ToString(hashBytes).Replace("-", string.Empty).ToLower();
+
+
+            //Generate newtel bearer token
+            var newtelToken = _configuration["Newtel:AccessKey"] + currentTime + hash;
+
+
+            //Initialize HttpClient for newtel request
+            var client = new HttpClient();
+            const string route = "https://api.new-tel.net/call-password/start-password-call";
+
+            client.DefaultRequestHeaders
+                .Accept
+                .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", newtelToken);
+
+            var stringContent = new StringContent(JsonConvert.SerializeObject(bodyAsString), Encoding.UTF8,
+                "application/json");
+
+            var response = await client.PostAsync(route, stringContent);
+            try
+            {
+                response.EnsureSuccessStatusCode();
+                var responseString = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Newtel api response: " + responseString);
+                return responseString;
+            }
+            catch (HttpRequestException)
+            {
+                throw new HttpRequestException(await response.Content.ReadAsStringAsync());
+            }
+        }
+    }
+}
